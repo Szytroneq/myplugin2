59485acdd5faf4c11d84ff3a7f6e42339c1c5971
Szytroneq <sebek.szlachta@gmail.com>
2025-07-06 16:47:21 +0200

    Add files via upload

---
+++ Myplugin/myplugin/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Exiled.API.Features;
+using Exiled.Loader;
+using player = Exiled.Events.Handlers.Player;
+
+namespace myplugin
+{
+    public class Class1 : Plugin<Config>
+    {
+        public static Class1 Instance;
+        public override void OnEnabled()
+        {
+            Instance = this;
+            player.Dying += EventHandlers.deathbroad.OnPlayerDeath;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Instance = null;
+            player.Dying -= EventHandlers.deathbroad.OnPlayerDeath;
+            base.OnDisabled();
+        }
+    }
+}
---
+++ Myplugin/myplugin/EventHandlers/deathbroad.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.Events.EventArgs.Player;
+using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myplugin.EventHandlers
+{
+    public static class deathbroad
+    {
+        public static void OnPlayerDeath(DyingEventArgs ev)
+        {
+            if (ev.Player.IsHuman)
+            {
+                // powiadomienie gdy człowiek umrze
+                Map.Broadcast(3, $"{ev.Player.Nickname} has died!");
+            }
+            else
+            {
+                // powiadomienie gdy nie człowiek umrze
+                Map.Broadcast(3, $"A non-human entity has died!");
+            }
+        }
+    }
+}
